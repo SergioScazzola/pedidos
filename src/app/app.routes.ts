7bf4041270f8237ce52cde8cc1d5c1257c516722
Sergio Scazzola <scazzolasergio@gmail.com>
2026-08-08 20:42:24 -0300

    inicio front pedidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NavegadorComponent } from './components/navegador/navegador.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+     
+  { path: 'ppal', component: NavegadorComponent },         
+                    
+  { path: '**', redirectTo: 'ppal' },         
+];
---
+++ src/app/components/navegador/navegador.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink,
+  RouterLinkActive, } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navegador',
+  imports: [RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: './navegador.component.html',
+  styleUrl: './navegador.component.css'
+})
+export class NavegadorComponent {
+  constructor(
+    private ruta   : ActivatedRoute,   
+    private router : Router
+  ) {}
+logout(): void {
+
+   localStorage.clear();
+  sessionStorage.clear();
+
+  // Ejecuta la orden para cerrar la solapa del navegador
+  window.close();
+  }
+
+}
